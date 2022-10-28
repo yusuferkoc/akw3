89c1e397ea4149522a5c0cf60553af63f5f739a6
yusuf <yusuferkoc99@gmail.com>
2022-10-29 02:38:45 +0300

    async ex

--- async.js
+++ async.js
@@ -1,39 +1,80 @@
-function getData (data) {
-    return new Promise((resolve,reject)=>{
-        console.log("Veriler alınıyor..")
-        
-        if(data) {
-            resolve("veriler alındı")
-        } else {
-            reject("veriler alınamadı")
-        }
-    })
+/*
+function getData(data) {
+  return new Promise((resolve, reject) => {
+    console.log("Veriler alınıyor..");
+
+    if (data) {
+      resolve("veriler alındı");
+    } else {
+      reject("veriler alınamadı");
+    }
+  });
 }
 
-function cleanData (receiveddata) {
-    return new Promise((resolve,reject)=>{
-        console.log("Veriler düzenleniyor..")
-        
-        if(receiveddata) {
-            resolve("veriler düzenlendi")
-        } else {
-            reject("veriler düzenlenemedi")
-        }
-    })
+function cleanData(receiveddata) {
+  return new Promise((resolve, reject) => {
+    console.log("Veriler düzenleniyor..");
+
+    if (receiveddata) {
+      resolve("veriler düzenlendi");
+    } else {
+      reject("veriler düzenlenemedi");
+    }
+  });
 }
 
 async function processData() {
-
-    try{
-        const receivedData = await getData(false);
-        console.log(receivedData)
-        const cleanedData = await cleanData(true);
-        console.log(cleanedData)
-    }
-    catch(error){
-        console.log(error)
-    }
-    }
-
+  try {
+    const receivedData = await getData(false);
+    console.log(receivedData);
+    const cleanedData = await cleanData(true);
+    console.log(cleanedData);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 processData();
+
+*/
+
+const books = [
+  { name: "Book1", author: "Author1" },
+  { name: "Book2", author: "Author2" },
+  { name: "Book3", author: "Author3" },
+];
+
+const listBooks = () => {
+  books.map((book) => {
+    console.log(book.name);
+  });
+};
+
+const addBook = (newBook) => {
+  const promise1 = new Promise((resolve, reject) => {
+    books.push(newBook);
+    //resolve(books)
+    reject("ERR");
+  });
+  return promise1;
+};
+
+/*addBook({name: "Book4" , author: "Author4"})
+  .then(() => {
+    console.log("Updated");
+    listBooks();
+  }).catch((error)=>{
+    console.log(error)
+  })
+  */
+
+async function showBooks() {
+  try {
+    await addBook({ name: "Book4", author: "Author4" });
+    listBooks();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+showBooks();
