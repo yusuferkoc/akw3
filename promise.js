e85c1c9aefdd8ab3ec378c501b00125305a16707
yusuf <yusuferkoc99@gmail.com>
2022-10-29 02:13:40 +0300

    promise ex

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-const pro = new Promise((resolve, reject) => {
+/* const pro = new Promise((resolve, reject) => {
   resolve("OK");
   reject("ERR");
 });
@@ -10,3 +10,35 @@ pro
   .catch((error) => {
     console.log(error);
   });
+
+*/
+
+const books = [
+  {name: "Book1", author: "Author1"},
+  {name: "Book2", author: "Author2"},
+  {name: "Book3", author: "Author3"}
+]
+
+const listBooks = () => {
+  books.map(book => {
+    console.log(book.name);
+  })
+}
+
+const addBook = (newBook) => {
+
+  const promise1 = new Promise((resolve ,reject) => {
+    books.push(newBook);
+    resolve(books)
+    reject("ERR")
+  })
+  return promise1;
+}
+
+addBook({name: "Book4" , author: "Author4"})
+.then(() => {
+  console.log("Updated");
+  listBooks();
+}).catch((error)=>{
+  console.log(error)
+})
